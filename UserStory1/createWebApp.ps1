ee77e7370e35eebb85e2d0b56c72d64121ff3421
Kevin <kevin.kurz@plan-b-gmbh.com>
2024-03-08 14:58:48 +0100

    edit parameter to make it work

---
+++ UserStory1/createWebApp.ps1
@@ -0,0 +1,21 @@
+param ([string] $parameters = $args[0])
+
+if ($parameters -eq 'production')
+{
+    Write-Host 'production parameter detected'
+    az deployment group create --resource-group rg-kevin-kurz --template-file UserStory1.bicep --parameters parameter.production.json
+}
+elseif ($parameters -eq 'test')
+{
+    Write-Host 'test parameter detected'
+    az deployment group create --resource-group rg-kevin-kurz --template-file UserStory1.bicep --parameters parameter.test.json
+}
+elseif ($parameters -eq 'development')
+{
+    Write-Host 'development parameter detected'
+    az deployment group create --resource-group rg-kevin-kurz --template-file UserStory1.bicep --parameters parameter.development.json
+}
+elseif ($parameters -eq 'world')
+{
+    Write-Host 'Hello, World!'
+}
