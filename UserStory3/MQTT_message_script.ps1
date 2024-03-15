22dcc4d80a06000a9a7aadb8645d20d3c9837b6c
Kevin <kevin.kurz@plan-b-gmbh.com>
2024-03-15 09:27:56 +0100

    change MQTT ps1 data

--- UserStory3/MQTT_message_script.ps1
+++ UserStory3/MQTT_message_script.ps1
@@ -3,21 +3,6 @@ $resourceGroup = "rg-kevin-kurz"
 $iotHubName = "userstory3Hub3ziviq2pzerxa"
 $deviceId = "KevinsDevice"
 $message = "Testnachricht von meinem Gerät"
-$consumerGroup = "\$Default"  # Standardverbrauchergruppe
 
-#$deleteDelayInSeconds = 60  # Anzahl der Sekunden, nach denen die Nachricht gelöscht wird
-
-# Meldung senden
 az iot device send-d2c-message --hub-name $iotHubName --device-id $deviceId --data $message
 
-# Nachrichten aus dem Event Hub abrufen
-# $events = az iot hub monitor-events --hub-name $iotHubName --device-id $deviceId --consumer-group $consumerGroup --timeout 30
-
-# # Überprüfen, ob die Testnachricht empfangen wurde
-# $received = $events -like "*$message*"
-
-# if ($received) {
-#     Write-Host "Die Testnachricht wurde erfolgreich empfangen."
-# } else {
-#     Write-Host "Die Testnachricht wurde nicht empfangen."
-# }
